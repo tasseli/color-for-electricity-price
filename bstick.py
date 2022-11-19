0a7c048ed25fb84c439d1f83bf3717f434ea5684
tasseli <tassel@iki.fi>
2022-11-20 01:08:57 +0200

    Added docs for my own later use

--- bstick.py
+++ bstick.py
@@ -1,3 +1,10 @@
+#Some reading if one is so inclined:
+#docs https://arvydas.github.io/blinkstick-python/
+#examples https://github.com/arvydas/blinkstick-python/wiki/BlinkStick-Pro%3A-Change-Color-of-a-Single-LED
+#examples etc home https://github.com/arvydas/blinkstick-python/wiki
+#blinkstick tutorials https://www.blinkstick.com/help/tutorials
+#updating electricity price, brought by Juhku https://byproductmusic.com/JH/nordpool/nordpool_just_nyt.html
+
 from blinkstick import blinkstick
 
 for bstick in blinkstick.find_all():
