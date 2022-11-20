d3ad4a3215bc3e7cc834ed5065fd2b17ca9bc278
tasseli <tassel@iki.fi>
2022-11-20 02:43:11 +0200

    Rewrote into a new function, set imports to beginning, double checked price is updated periodically

--- bstick.py
+++ bstick.py
@@ -6,9 +6,12 @@
 #updating electricity price, brought by Juhku https://byproductmusic.com/JH/nordpool/nordpool_just_nyt.html
 
 from blinkstick import blinkstick
+from urllib import request
+import ssl
+import time, sys
 
 for bstick in blinkstick.find_all():
-    print ("Found device:")    
+    print ("Found device:")
     print ("    Manufacturer:  " + bstick.get_manufacturer())
     print ("    Description:   " + bstick.get_description())
     print ("    Serial:        " + bstick.get_serial())
@@ -19,19 +22,24 @@ bstick = blinkstick.find_first()
 bstick.set_info_block1("Electricity price BlinkStick")
 print (bstick.get_info_block1())
 
-import ssl
 context = ssl._create_unverified_context()
+line10 = '0'
 
-from urllib import request 
-link = "https://byproductmusic.com/JH/nordpool/nordpool_just_nyt.html"
-f = request.urlopen(link, context=context)
-myfile = f.read()
-lineiterator = iter(myfile.splitlines())
-for i in range(0,9):
-    next(lineiterator)
-line10 = next(lineiterator)
-line10 = line10.decode("utf-8")
-print(line10)
+def update_price():
+
+    link = "https://byproductmusic.com/JH/nordpool/nordpool_just_nyt.html"
+    f = request.urlopen(link, context=context)
+    myfile = f.read()
+    lineiterator = iter(myfile.splitlines())
+    for i in range(0,9):
+        next(lineiterator)
+    line10 = next(lineiterator)
+    line10 = line10.decode("utf-8")
+    print(line10)
+    f.close()
+    sys.stdout.flush()
+
+update_price()
 
 def update_color():
     if (float(line10) > 40):
@@ -40,11 +48,11 @@ def update_color():
         bstick.set_color(name="orange")
     if (float(line10) < 20):
         bstick.set_color(name="green")
+    sys.stdout.flush()
 
 update_color()
 
-import time, sys
 while (True):
-    sys.stdout.flush()
     time.sleep(60*60)
+    update_price()
     update_color()
